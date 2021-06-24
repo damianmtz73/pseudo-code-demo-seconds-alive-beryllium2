27633b9fafebd8e96a7a7da26cc517e20b7b47fb
damian martinez <damianmtz9@gmail.com>
2021-06-24 12:48:16 -0400

    created inputs

---
+++ main.js
@@ -0,0 +1,4 @@
+let yearOfBirthInput= prompt("Enter the year of your birth.")
+let monthOfBirthInput= prompt("Enter the month you were born in.")
+let monthOfBirthInput= parseFloat(monthOfBirth)
+let yearOfBirthInput= parseFloat(yearOfBirth)
