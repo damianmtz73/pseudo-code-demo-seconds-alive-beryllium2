babc7372f9c298c20f5095b4e7f04ee4f0f23f2c
damian martinez <damianmtz9@gmail.com>
2021-06-24 18:00:31 -0400

    finish Demo

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 let yearOfBirthInput= prompt("Enter the year of your birth.")
 let monthOfBirthInput= prompt("Enter the month you were born in.")
-let monthOfBirthInput= parseFloat(monthOfBirth)
-let yearOfBirthInput= parseFloat(yearOfBirth)
+let currentMonth= new Date().getMonth()+1
+let currentYear= new Date().getFullYear()
+
+console.log({yearOfBirthInput})
+console.log({monthOfBirthInput})
+console.log({currentMonth})
+console.log({currentYear})
+
+let monthOfBirth= parseInt(monthOfBirthInput)
+let yearOfBirth= parseInt(yearOfBirthInput)
+
+
+console.log({currentYear})
+console.log({currentMonth})
+
 let secInMonth= 60*60*24*30
 let secInYear= secInMonth*12
 let secBirthYear= secInMonth*(12-monthOfBirth)
-let secCurrentYear= secInMonth*Data
+let secCurrentYear= secInMonth*currentMonth
+let secondsInBetween= secInYear* (currentYear-yearOfBirth-2)
+let secondsSinceBirth= secBirthYear+secondsInBetween+secCurrentYear
+console.log({secondsSinceBirth})
+
+document.write(`<h3>You have been born for ${(secondsSinceBirth).toLocaleString()}seconds approximately.</h3>`)
