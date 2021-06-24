70ca455484f01757c7c26dac7e2cecd305731cdf
damian martinez <damianmtz9@gmail.com>
2021-06-24 12:55:13 -0400

    add month and year variables

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ let yearOfBirthInput= prompt("Enter the year of your birth.")
 let monthOfBirthInput= prompt("Enter the month you were born in.")
 let monthOfBirthInput= parseFloat(monthOfBirth)
 let yearOfBirthInput= parseFloat(yearOfBirth)
+let secInMonth= 60*60*24*30
+let secInYear= secInMonth*12
+let secBirthYear= secInMonth*(12-monthOfBirth)
+let secCurrentYear= secInMonth*Data
